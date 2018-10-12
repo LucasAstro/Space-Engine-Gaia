9c1acdc27d3bb64d354121546e60140f54634dfd
LucasAstro <airmasterdude@gmail.com>
2018-10-12 06:35:29 -0500

    Multitude of changes...
    Major performance increase

--- Gaia.py
+++ Gaia.py
@@ -1,92 +1,76 @@
-import zipfile
+import numpy
+import astropy
 import os
-import shutil
 from astropy.io.votable import parse
 from astropy.coordinates import ICRS
 from astropy import units as u
+myinput = input("What is the file you want to read" + "(" + "must have .vot in the name!" + ")")
+dirpath = os.getcwd()
 
-import numpy
-cwd = os.getcwd()
-whichline = 0
-# Credit goes to Watsisname for help with a lot of equations!
-votable = parse("result.vot")
-file = open("yet" + ".txt", 'w+')
-for resource in votable.resources:
-    for table in resource.tables:
-        arrayID = table.array['source_id']
-        indivID = numpy.split(arrayID, len(arrayID))
-        for i in indivID:
-            whichline = whichline + 1
-            print(str(whichline) + "/" + "5995")
-            file = open(str(whichline) + ".sc", 'w+')
+votable = parse(myinput)
+newpath = str(dirpath + "\\outputsc")
+if os.path.exists(newpath):
+    os.chdir(newpath)
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+    os.chdir(newpath)
+# Credit goes to Watsisname and Grote for a few bits of code and help with some equations
+numRow = 0
+for table in votable.iter_tables():
+    arrayID = table.array['source_id']
+    for i in arrayID:
+        numRow = numRow + 1
+        with open(str(numRow) + ".sc", 'w+') as file:
             arrayRA = table.array['ra']
-            indivRA = numpy.split(arrayRA, len(arrayRA))
             
             arrayID = table.array['source_id']
-            indivID = numpy.split(arrayID, len(arrayID))
 
             arrayDEC = table.array['dec']
-            indivDEC = numpy.split(arrayDEC, len(arrayDEC))
             
-            c = ICRS(indivRA[whichline-1]*u.degree, indivDEC[whichline-1]*u.degree)
+            c = ICRS(arrayRA[numRow-1]*u.degree, arrayDEC[numRow-1]*u.degree)
             
             rahmsstr = c.ra.to_string(u.hour)
             decdmsstr = c.dec.to_string(u.degree, alwayssign=True)
             arraydist = table.array['r_est']
-            distance = arraydist[whichline-1]
+            distance = arraydist[numRow-1]
             
             arrayTeff = table.array['teff_val']
-            indivTeff = numpy.split(arrayTeff,len(arrayTeff))
 
             arrayRadSol = table.array['radius_val']
-            indivRadSol = numpy.split(arrayRadSol, len(arrayRadSol))
 
             arrayLum = table.array['lum_val']
-            indivLum = numpy.split(arrayLum, len(arrayLum))
-            teffoutput = str(indivTeff[whichline-1])
+            teffoutput = str(arrayTeff[numRow-1])
 
-            teffoutput = teffoutput.replace("[", "")
-            teffoutput = teffoutput.replace("]", "")
 
-            radsoloutput = str(indivRadSol[whichline-1])
+            radsoloutput = str(arrayRadSol[numRow-1])
 
-            radsoloutput = radsoloutput.replace("[", "")
-            radsoloutput = radsoloutput.replace("]", "")
 
-            Lumoutput = str(indivLum[whichline-1])
+            Lumoutput = str(arrayLum[numRow-1])
 
-            Lumoutput = Lumoutput.replace("[", "")
-            Lumoutput = Lumoutput.replace("]", "")
 
-            rahoutput = numpy.array_str(rahmsstr)
+            rahoutput = str(rahmsstr)
             
             rahoutput = rahoutput.replace("h", " ")
             rahoutput = rahoutput.replace("m", " ")
             rahoutput = rahoutput.replace("s", "")
-            rahoutput = rahoutput.replace("[", "")
-            rahoutput = rahoutput.replace("]", "")
             rahoutput = rahoutput.replace("'", "")
             
-            decoutput = numpy.array_str(decdmsstr)
+            decoutput = str(decdmsstr)
             
             decoutput = decoutput.replace("d", " ")
             decoutput = decoutput.replace("m", " ")
             decoutput = decoutput.replace("+", "")
             decoutput = decoutput.replace("s", "")
-            decoutput = decoutput.replace("[", "")
-            decoutput = decoutput.replace("]", "")
             decoutput = decoutput.replace("'", "")
-            file.write("Star" + "\t" + '"' + str(whichline-1) + '"' + "\n{")
-           
-            file.write("\n \t \tRA " + str(rahoutput))
-            file.write("\n \t \tDec " + str(decoutput))            
-            file.write("\n \t \tDist " + str(distance))
-            file.write("\n \t \tClass  " + '"' + "G5V" + '"')
-            file.write("\n \t \tLum " + Lumoutput)
-            file.write("\n \t \tRadSol " + radsoloutput)
-            file.write("\n \t \tMassSol " + "1")
-            file.write("\n \t \tTeff " + teffoutput)
-            file.write("\n \t")
-            file.write("\n}")
-            file.close()
-        pass
+            file.write("Star" + "\t" + '"' + str(numRow-1) + '"' + "\n{"
+                      + "\n \t \tRA " + rahoutput + 
+                      "\n \t \tDec " + decoutput + 
+                     "\n \t \tDist " + str(distance) + 
+                     "\n \t \tClass  " + '"' + "G5V" + '"' + 
+                     "\n \t \tLum " + Lumoutput + 
+                     "\n \t \tRadSol " + radsoloutput + 
+                     "\n \t \tMassSol 1" + 
+                     "\n \t \tTeff " + teffoutput + 
+                     "\n \t" + "\n}")
+        os.path.join(newpath, str(numRow) + ".sc")
+print("Completed!")
