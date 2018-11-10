46a6d04b87387f8731b51e586ad8211d38513d6f
LucasAstro <airmasterdude@gmail.com>
2018-11-10 14:40:14 -0600

    Phunnie helped fix this all

--- Gaia.py
+++ Gaia.py
@@ -1,6 +1,8 @@
 import os
 import math
 import random
+import astropy
+import numpy
 from astropy.io.votable import parse
 from astropy.coordinates import ICRS
 from astropy import units as u
@@ -43,141 +45,7 @@ def calcsspectral(temperature):
 numRow = 0
 for table in votable.iter_tables():
     idss = 0
-    arrayDesignation = table.array['designation']
-import os
-import math
-import random
-from astropy.io.votable import parse
-from astropy.coordinates import ICRS
-from astropy import units as u
-myinput = input("What is the file you want to read" + "(" + "must have .vot in the name!" + ")\n")
-dirpath = os.getcwd()
-
-votable = parse(myinput)
-newpath = str(dirpath + "\\outputsc")
-if os.path.exists(newpath):
-    os.chdir(newpath)
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
-    os.chdir(newpath)
-if(os.path.isfile(newpath + "\\outputsc.csv")):
-    os.remove("outputsc.csv")
-       
-# Credit goes to Watsisname, and Grote for a few bits of code and help with some equations!
-# Also, thanks to Phunnie for some help too!
-# Thanks to DominikDoom too for some enhancements!
-def calcsspectral(temperature):
-    if(temperature <= 3850):
-        return "M"
-    if(temperature > 3850):
-        if(temperature <= 5300):
-            return "K"
-    if(temperature > 5300):
-        if(temperature <= 5920):
-            return "G"
-    if(temperature > 5920):
-        if(temperature <= 7240):
-            return "F"
-    if(temperature > 7240):
-        if(temperature <= 9500):
-            return "A"
-    if(temperature > 9500):
-        if(temperature <= 31000):
-            return "B"
-    if(temperature > 31000):
-            return "O"
-numRow = 0
-for table in votable.iter_tables():
-    idss = 0
-    arrayDesignation = table.array['designation']
-
-    for i in arrayDesignation:
-        numRow = numRow + 1
-        with open("outputsc" + ".csv", 'a+') as file:
-            if idss == 0:
-                idss = 1
-                
-                file.write("Name,RA,Dec,Dist,AppMagn,SpecClass,MassSol,RadSol,Temperature\n")
-                
-            
-            arrayRA = table.array['ra']
-            
-            arrayDEC = table.array['dec']
-            
-            c = ICRS(arrayRA[numRow-1]*u.degree, arrayDEC[numRow-1]*u.degree)
-            
-            rahmsstr = c.ra.to_string(u.hour)
-            decdmsstr = str(arrayDEC[numRow-1])
-            arraydist = table.array['r_est']
-            distance = arraydist[numRow-1]
-            
-            arrayTeff = table.array['teff_val']
-                
-            arrayRadSol = table.array['radius_val']
-
-            arrayLum = table.array['lum_val']
-            teffoutput = str(arrayTeff[numRow-1])
-
-            radsoloutput = str(arrayRadSol[numRow-1])
-            
-            Lumoutput = str(arrayLum[numRow-1])
-
-            designationoutput = str(arrayDesignation[numRow-1])
-            rahoutput = str(rahmsstr)
-            
-            rahoutput = rahoutput.replace("h", " ")
-            rahoutput = rahoutput.replace("m", " ")
-            rahoutput = rahoutput.replace("s", "")
-            
-            decoutput = str(decdmsstr)
-            decoutput = decoutput.replace("d", " ")
-            decoutput = decoutput.replace("m", " ")
-            decoutput = decoutput.replace("+", "")
-            decoutput = decoutput.replace("s", "")
-            designationoutput = designationoutput.replace("b'", "")
-            designationoutput = designationoutput.replace("'", "")
-            absolute_magnitude = 4.83 - 2.5 * math.log10(float(Lumoutput))
-            apparent_magnitude = absolute_magnitude + 5 * math.log10(float(distance)) - 5
-            file.write(designationoutput + "," + str(rahoutput) + "," + str(decoutput) + "," + str(distance) + "," + str(apparent_magnitude) + "," + str(calcsspectral(float(teffoutput)) + str(random.randrange(1,9)) + "V") + "," + "," + str(radsoloutput) + "," + str(teffoutput) + "\n")
-            
-        os.path.join(newpath, str(numRow) + ".csv")
-print("\nCompleted!")
-newpath = str(dirpath + "\\outputsc")
-if os.path.exists(newpath):
-    os.chdir(newpath)
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
-    os.chdir(newpath)
-if(os.path.isfile(newpath + "\\outputsc.csv")):
-    os.remove("outputsc.csv")
-       
-# Credit goes to Watsisname, and Grote for a few bits of code and help with some equations!
-# Also, thanks to Phunnie for some help too!
-# Thanks to DominikDoom too for some enhancements!
-def calcsspectral(temperature):
-    if(temperature <= 3850):
-        return "M"
-    if(temperature > 3850):
-        if(temperature <= 5300):
-            return "K"
-    if(temperature > 5300):
-        if(temperature <= 5920):
-            return "G"
-    if(temperature > 5920):
-        if(temperature <= 7240):
-            return "F"
-    if(temperature > 7240):
-        if(temperature <= 9500):
-            return "A"
-    if(temperature > 9500):
-        if(temperature <= 31000):
-            return "B"
-    if(temperature > 31000):
-            return "O"
-numRow = 0
-for table in votable.iter_tables():
-    idss = 0
-    arrayID = table.array['source_id']
+    arrayID = table.array['designation']
 
     for i in arrayID:
         numRow = numRow + 1
@@ -187,11 +55,9 @@ for table in votable.iter_tables():
                 idss = 1
                 
                 file.write("Name,RA,Dec,Dist,AppMagn,SpecClass,MassSol,RadSol,Temperature\n")
-                
-            
             arrayRA = table.array['ra']
             
-            arrayID = table.array['source_id']
+            arrayID = table.array['designation']
 
             arrayDEC = table.array['dec']
             
@@ -200,7 +66,8 @@ for table in votable.iter_tables():
             rahmsstr = c.ra.to_string(u.hour)
             decdmsstr = str(arrayDEC[numRow-1])
             arraydist = table.array['r_est']
-            #arrayDesignation = table.array['designation']
+            arrayDesignation = table.array['designation']
+            designation = arrayDesignation.astype(str)
             distance = arraydist[numRow-1]
             
             arrayTeff = table.array['teff_val']
@@ -214,7 +81,7 @@ for table in votable.iter_tables():
             
             Lumoutput = str(arrayLum[numRow-1])
 
-            #designationoutput = str(arrayDesignation[numRow-1])
+            designationoutput = str(designation[numRow-1])
             rahoutput = str(rahmsstr)
             
             rahoutput = rahoutput.replace("h", " ")
@@ -222,10 +89,6 @@ for table in votable.iter_tables():
             rahoutput = rahoutput.replace("s", "")
             
             decoutput = str(decdmsstr)
-            
-            degreerah = rahoutput.split(" ")
-            
-            degreeoutput = 15*int(degreerah[0])+int(degreerah[1])/4+float(degreerah[2])/240
 
             decoutput = decoutput.replace("d", " ")
             decoutput = decoutput.replace("m", " ")
@@ -233,7 +96,7 @@ for table in votable.iter_tables():
             decoutput = decoutput.replace("s", "")
             absolute_magnitude = 4.83 - 2.5 * math.log10(float(Lumoutput))
             apparent_magnitude = absolute_magnitude + 5 * math.log10(float(distance)) - 5
-            file.write(str(numRow) + "," + str(degreeoutput) + "," + str(decoutput) + "," + str(distance) + "," + str(apparent_magnitude) + "," + str(calcsspectral(float(teffoutput)) + str(random.randrange(1,9)) + "V") + "," + "," + str(radsoloutput) + "," + str(teffoutput) + "\n")
+            file.write(str(designationoutput) + "," + str(rahoutput) + "," + str(decoutput) + "," + str(distance) + "," + str(apparent_magnitude) + "," + str(calcsspectral(float(teffoutput)) + str(random.randrange(1,9)) + "V") + "," + "," + str(radsoloutput) + "," + str(teffoutput) + "\n")
             
         os.path.join(newpath, str(numRow) + ".csv")
 print("\nCompleted!")
