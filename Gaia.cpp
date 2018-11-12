bdf6ba7a3999fc0f067995df8d80651f9cc8614e
LucasAstro <airmasterdude@gmail.com>
2018-11-12 11:55:06 -0600

    Update Gaia.cpp

--- Gaia.cpp
+++ Gaia.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #include <iostream>
 #include "csv.h"
-#include <time.h>
 #include <string>
 #include <vector>
 #include <math.h>
@@ -35,8 +34,6 @@ std::string calcsspectral(float temperature) {
 int csvlinecount(std::string csv_file) {
 	int count = 0;
 
-	srand(time(NULL));
-
 	std::string line;
 	std::ifstream file(csv_file);
 
@@ -52,13 +49,12 @@ int csvlinecount(std::string csv_file) {
 	std::vector<float> radiusarray(count - 1);
 	std::vector<float> magnitudearray(count - 1);
 	std::vector<std::string> designationarray(count - 1);
-
 	io::CSVReader<7> in(csv_file);
 	std::ofstream myfile("output.csv");
 	int newcount = 0;
 	int csvcount = 0;
 	in.read_header(io::ignore_extra_column, "ra", "dec", "r_est", "teff_val", "radius_val", "phot_g_mean_mag" /*"radius_val", lum_val,*/, "designation");
-	double ra; double dec; double r_est; float teff_val; float radius_val; float phot_g_mean_mag; /*; float lum_val*/ std::string designation;;
+	double ra; double dec; double r_est; float teff_val; float radius_val; float phot_g_mean_mag; /*; float lum_val*/ std::string designation;
 	while (in.read_row(ra, dec, r_est, teff_val, radius_val, phot_g_mean_mag /*radius_val, lum_val*/, designation)) {
 		csvcount++;
 		designationarray[csvcount - 1] = designation;
@@ -67,18 +63,14 @@ int csvlinecount(std::string csv_file) {
 		distarray[csvcount - 1] = r_est;
 		teffarray[csvcount - 1] = teff_val;
 		radiusarray[csvcount - 1] = radius_val;
-		magnitudearray[csvcount - 1] = phot_g_mean_mag /* = lum_val*/;
+		magnitudearray[csvcount - 1] = phot_g_mean_mag; /* = lum_val*/
 		if (myfile.is_open())
 		{
 			if(csvcount == 1)
 			{
 				myfile << "Name,RA,Dec,Dist,AppMagn,SpecClass,MassSol,RadSol,Temperature" << std::endl;
 			}
-			/*
-			float absolute_magnitude = 4.83 - 2.5 * log10(magnitudearray[csvcount - 1]);
-			float apparent_magnitude = absolute_magnitude + 5 * log10(distarray[csvcount - 1]) - 5;
-			*/
-			myfile << designationarray[csvcount - 1] << "," << raarray[csvcount-1] << "," << decarray[csvcount - 1] << "," << distarray[csvcount - 1] << "," << magnitudearray[csvcount -1] << "," << calcsspectral(teff_val) << (rand() % 9 + 1) << "V" << "," << "," << radiusarray[csvcount - 1] << "," << teff_val << std::endl;
+			myfile << designationarray[csvcount - 1] << "," << pmraarray[csvcount-1] << "," << pmdecarray[csvcount-1] << "," << distarray[csvcount - 1] << "," << magnitudearray[csvcount -1] << "," << calcsspectral(teff_val) << (rand() % 9 + 1) << "V" << "," << "," << radiusarray[csvcount - 1] << "," << teff_val << std::endl;
 			newcount++;
 			std::cout << newcount << std::endl;	
 		}
@@ -93,7 +85,5 @@ int csvlinecount(std::string csv_file) {
 
 int main() 
 {
-	std::string inputfile;
-	std::cin >> inputfile;
-	csvlinecount(inputfile);
+	csvlinecount("ram.csv");
 }
